dbaf02b149d972468d8b29dc4b19153be12bae94
GloyseC <80674591+GloyseC@users.noreply.github.com>
2021-12-05 12:16:28 +0530

    updated signup file

--- Signup.java
+++ Signup.java
@@ -1,4 +1,4 @@
-
+/*
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -81,5 +81,72 @@ public class Signup {
 		   }//end try
 
 		
+	}
+}
+*/
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Signup {
+	static Integer idval =0;
+	public void sign(Scanner scan){
+		Connection conn = null;
+		Statement stmt = null;
+		try{
+		      Class.forName("com.mysql.jdbc.Driver");
+		      conn = DriverManager.getConnection("jdbc:mysql://Localhost:3306/Gunshop", "root", "dbms");
+		      stmt = conn.createStatement();
+		      System.out.println("\t\t\t\t\tSIGN UP PAGE ");
+		      try {
+	                  String name = "";
+        	          String password = "";
+                	  String email = "";
+		  	  int custid = "";
+		  	  Stirng phnnum = "";
+		    	  System.out.println("ENTER YOUR NAME -");
+		    	  name = scan.nextLine();
+		    	  
+		    	  System.out.println("SET YOUR PASSWORD -");
+		    	  password = scan.nextLine();
+		    	  
+		    	  System.out.println("ENTER YOUR OFFICIAL EMAIL ID -");
+		    	  Email = scan.nextLine();
+		    	  
+		    	  System.out.println("ENTER YOUR PHONE NUMBER -");
+		    	  phnnum = scan.nextLine();
+
+		    	  idval=idval+1;
+		    	  String sql = "INSERT INTO user VALUES ("+idval+ ", " + "'" + password + "', " + "'" + name + "', " + "'" + mail + "', "+"'" + phnnum +"');";
+		    	  stmt.execute(sql);
+		    	  System.out.println("SUCCESSFULLY ADDED");
+		    	  System.out.println("DEFAULT ID ASSIGNED TO YOU IS :");
+		    	  System.out.println(idval);
+		      }
+		      catch (Exception e)
+		      {
+		    	  System.out.println(e);
+		      }
+		      
+		}
+		catch(SQLException | ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		finally{
+		      try{
+		         if(stmt!=null)
+		            conn.close();
+		      }catch(SQLException se){}
+		      try{
+		         if(conn!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		         se.printStackTrace();
+		         System.out.println("Error closing connection to the database!");
+		      }
+		   }
 	}
 }
