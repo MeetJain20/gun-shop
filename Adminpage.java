5af09d56239dc5cc27513598fece98ebdac3213f
GloyseC <80674591+GloyseC@users.noreply.github.com>
2021-12-05 22:34:08 +0530

    updated

--- Adminpage.java
+++ Adminpage.java
@@ -1,4 +1,4 @@
-
+/*
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -195,6 +195,175 @@ public class Adminpage {
 		         System.out.println("Error closing connection to the database!");
 		      }//end finally try
 		   }//end try
+		return;
+	}
+
+}
+*/
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Adminpage {
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://Localhost:3306/test";
+	static final String USER = "Enter your username";
+	static final String PASS = "Enter Your password";
+
+	public void adpage(Scanner scan){
+		Connection conn = null;
+		Statement stmt = null;
+		try{
+		      Class.forName(JDBC_DRIVER);
+		      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		      stmt = conn.createStatement();
+		      
+		      boolean querying = true;
+		      System.out.printf("\n\n- - - -╾━╤デ╦︻  LOGIN PAGE  ︻╦デ╤━╾- - - -\n\n\n");
+		      
+		      try  {
+		    	  while(querying)
+ 
+ 		           {
+		               //Print the menu
+		    		   System.out.println();
+		        	   System.out.printf("\n********************************\n");
+		               System.out.printf("Please select a query below!\n\n");		       
+		               System.out.printf("1. Enter New Model\n");
+		               System.out.printf("2. Update Price\n");
+		               System.out.printf("3. Update Availablity\n");
+		               System.out.printf("4. Delete Model\n");
+		               System.out.printf("5. Place Order\n");
+		               System.out.printf("6. Logout\n");
+		               System.out.printf("\n********************************\n");
+		               System.out.println();
+		               
+		               System.out.printf("\nEnter Your Choice\n");
+		               String input = scan.nextLine();
+		               int answer;
+		               try{
+		            	   answer=Integer.valueOf(input);
+		               }
+		               catch(Exception e)
+		               {
+		            	   answer=-1;
+		               }
+		               if(answer == -1 || answer < 1 || answer > 6)
+		               {
+		                   //User entered incorrect input
+		                   System.out.printf("\nIncorrect Input!\n");
+		                   System.out.printf("Please enter a number 1-6\n");
+		               }
+		               
+		               else if (answer==1)
+		               {
+		            	   String Model = "";
+		                   String Details = "";
+		                   Integer Price = 0;
+		                   Integer Avail = 0;
+		 		    	   System.out.printf("\nEnter Model No\n");
+		 		    	   Model = scan.nextLine();
+		 		    	  
+		 		    	   System.out.printf("Enter Details\n");
+		 		    	   Details = scan.nextLine();
+		 		    	  
+		 		    	   System.out.printf("Enter Price\n");
+		 		    	   Price = scan.nextInt();
+		 		    	  
+		 		    	   System.out.printf("Enter Availablity\n");
+		 		    	   Avail = scan.nextInt();
+		 		    	   String sql = "INSERT INTO wm VALUES " + "( '"+ Model + "' , " + "'" + Details + "', " + "" + Price + ", " + "" + Avail + ")";
+				    	   stmt.execute(sql);
+				    	   System.out.printf("\nSuccess!\n");
+		               }
+		               
+		               else if (answer==2)
+		               {
+		            	   String Model = "";  
+		                   Integer Price = 0;
+		                 
+		 		    	   System.out.printf("\nEnter Model No\n");
+		 		    	   Model = scan.nextLine();
+		 		    	  
+		 		    	   System.out.printf("Enter New Price\n");
+		 		    	   Price = scan.nextInt();
+		 		    	  
+		 		    	   String sql1 = "UPDATE wm SET wm.Price = ? where wm.Title=?";
+	                	   PreparedStatement stmt1 = conn.prepareStatement(sql1);
+	                	   stmt1.setInt(1, Price);
+	                	   stmt1.setString(2, Model);
+	                	   stmt1.executeUpdate();
+				    	   System.out.printf("\nSuccess!\n");
+		               }
+		               
+		               else if (answer==3)
+		               {
+		            	   String Model = "";  
+		                   Integer Avail = 0;
+		                 
+		 		    	   System.out.printf("\nEnter Model No\n");
+		 		    	   Model = scan.nextLine();
+		 		    	  
+		 		    	   System.out.printf("Enter New Availablity\n");
+		 		    	   Avail = scan.nextInt();
+		 		    	  
+		 		    	   String sql1 = "UPDATE wm SET wm.Availablity = ? where wm.Title=?";
+	                	   PreparedStatement stmt1 = conn.prepareStatement(sql1);
+	                	   stmt1.setInt(1, Avail);
+	                	   stmt1.setString(2, Model);
+	                	   stmt1.executeUpdate();
+				    	   System.out.printf("\nSuccess!\n");
+		               }
+		               else if (answer==4)
+		               {
+		            	   String Model = "";  
+		 		    	   System.out.printf("\nEnter Model No\n");
+		 		    	   Model = scan.nextLine();
+		 		    	   String sqlDel = "DELETE FROM wm WHERE LOWER(Title) LIKE LOWER('%" + Model + "%')";
+	                       stmt.execute(sqlDel);
+	                       System.out.printf("\nModel deleted!\n");
+		               }
+		               else if (answer==5){
+		            	   String Model = "";  
+		 		    	   System.out.printf("\nEnter Model No\n");
+		 		    	   Model = scan.nextLine();
+		 		    	   String sqlDel = "DELETE FROM book WHERE LOWER(Model) LIKE LOWER('%" + Model + "%')";
+	                       stmt.execute(sqlDel);
+	                       System.out.printf("\nModel Ordered!\n");
+		               }
+		               else if (answer==6)
+		               {
+		            	   querying= false;
+		               }
+		           }
+		      }
+		    	  catch (Exception e)
+		      {
+		    	  System.out.println(e);
+		      }
+		}
+		catch(SQLException | ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		finally{
+		      try{
+		         if(stmt!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		      }
+		      try{
+		         if(conn!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		         se.printStackTrace();
+		         System.out.printf("\nError closing connection to the database!\n");
+		      }
+		   }
 		return;
 	}
 
