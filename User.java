9a37c85b00a57efe7885fd817ee0d1d1cab49c27
Anand Thakur <75462622+Appleblade22@users.noreply.github.com>
2021-12-05 12:23:34 +0530

    Update User.java
    ok

--- User.java
+++ User.java
@@ -1,5 +1,5 @@
 
-
+package Gun_Shop;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -13,11 +13,11 @@ import java.util.Scanner;
 public class User {
 	
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-	static final String DB_URL = "jdbc:mysql://Localhost:3306/test";
-	static final String USER = "Username";
-	static final String PASS = "Passwor";
+	static final String URL = "jdbc:mysql://Localhost:3306/Gunshop";
+	static final String USER = "root";
+	static final String PASS = "dbms";
 
-	public void fun(Scanner scan, Integer Id1)
+	public void fun(Scanner sc, Integer Id1)
 	{
 		Connection conn = null;
 		Statement stmt = null;
@@ -27,7 +27,7 @@ public class User {
 
 		      //STEP 2: Open a connection
 		      System.out.println("Connecting to a selected database...");
-		      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		      conn = DriverManager.getConnection(URL, USER, PASS);
 		      System.out.println("Connected database successfully...");
 		      
 		      //STEP 3: Execute a query
@@ -36,22 +36,22 @@ public class User {
 		      
 		      
 		      //Welcome user!
-		      System.out.println("Welcome to the Washing Machine Purchasing Program!");
+		      System.out.println("Welcome to the Lock'n Loaded");
 		      
 		      //Initialize our querying variable
-		      boolean querying = true;
+		      boolean flag = true;
 		      
 		      try  {
-		           while(querying)
+		           while(flag)
 		           {
 		               //Print the menu
 		        	   System.out.println();
 		        	   System.out.println("********************************");
-		               System.out.println("Please select a query below!");
+		               System.out.println("Please choose an option->");
 		               //5 available options
-		               System.out.println("1. List of all Models of  Washing Machine");
-		               System.out.println("2. List of Details of Model ");
-		               System.out.println("3. Purchase Washine Machine");
+		               System.out.println("1. List of all Fire Arms Models");
+		               System.out.println("2. Model Details");
+		               System.out.println("3. Purchase Fire Arms");
 		               System.out.println("4. Check Your Orders");
 		               System.out.println("5. Dashboard");
 		               System.out.println("6. Logout");
@@ -59,35 +59,35 @@ public class User {
 		               System.out.println();
 		               
 		               System.out.println("Enter Your Choice");
-		               String input = scan.nextLine();
-		               int answer;
+		               String input = sc.nextLine();
+		               int reply;
 		               try{
-		            	   answer=Integer.valueOf(input);
+		            	   reply=Integer.valueOf(input);
 		               }
 		               catch(Exception e)
 		               {
-		            	   answer=-1;
+		            	   reply=-1;
 		               }
-		               if(answer == -1 || answer < 1 || answer > 5)
+		               if(reply == -1 || reply < 1 || reply > 5)
 		               {
 		                   //User entered incorrect input
 		                   System.out.println("Incorrect Input!");
 		                   System.out.println("Please enter a number 1-5");
 		               }
 		               
-		               else if (answer == 1)
+		               else if (reply == 1)
 		               {
-		            	   String sql = "SELECT Title FROM wm";
+		            	   String sql = "SELECT Gunmodel FROM model";
 		            	   
 		                   try (ResultSet rs = stmt.executeQuery(sql)) {
 		                       
-		                       System.out.println("Models:");
+		                       System.out.println("Gun Models:");
 		                       while(rs.next()){
 		                           //Retrieve by column name
-		                           String title = rs.getString("Title");
+		                           String Gunmodel = rs.getString("Gunmodel");
 		                           
 		                           //Display values
-		                           System.out.println(title);
+		                           System.out.println(Gunmodel);
 		                       }
 		                       
 		                       //Close the result set
@@ -100,28 +100,28 @@ public class User {
 		               }
 		               
 		               
-		               else if (answer == 2)
+		               else if (reply == 2)
 		               {
-		            	   System.out.println("Please enter the Model Name");
+		            	   System.out.println("Please enter the FireArm Model ->");
 		                   
 		            	  
-		                   input = scan.nextLine();
+		                   input = sc.nextLine();
 		                   
 		                   //Query the DB
-		                   String sql = "SELECT * FROM wm WHERE LOWER(Title) LIKE LOWER('%" + input + "%')";
+		                   String sql = "SELECT * FROM model WHERE LOWER(Gunmodel) LIKE LOWER('%" + input + "%')";
 		                   ResultSet rs = stmt.executeQuery(sql);
 		                   
 		                   if(rs.next())
 		                   {
 		                       //Retrieve by column name
-		                       String title = rs.getString("Title");
-		                       String details = rs.getString("Details");
-		                       String Price = rs.getString("Price");
-		                       String Availablity = rs.getString("Availablity");
+		                       String Gunmodel = rs.getString("Gunmodel");
+		                       String details = rs.getString("details");
+		                       String Price = rs.getString("price");
+		                       String Availablity = rs.getString("availability");
 		                       
 		                       //Display values
 		                       System.out.print("Model: ");
-		                       System.out.println(title);
+		                       System.out.println(Gunmodel);
 		                       System.out.print("Details: ");
 		                       System.out.println(details);
 		                       System.out.print("Price: ");
@@ -132,70 +132,69 @@ public class User {
 		                   }
 		                   else
 		                   {
-		                       System.out.println("That model does not exist...");
+		                       System.out.println("This FireArm is not available...");
 		                   }
 		                   
 		                   rs.close();
 		               }
 		               
 		               
-		               else if (answer==3)
+		               else if (reply==3)
 		               {
-		            	   System.out.println("Please enter the Model Name");
-		                   input = scan.nextLine();
-		                   String sql = "SELECT Availablity FROM wm WHERE LOWER(Title) LIKE LOWER('%" + input + "%')";
+		            	   System.out.println("Please Enter FireArm Model ->");
+		                   input = sc.nextLine();
+		                   String sql = "SELECT availablity FROM model WHERE LOWER(Gunmodel) LIKE LOWER('%" + input + "%')";
 		                   ResultSet rs = stmt.executeQuery(sql);
 		                 
 		                   if(rs.next())
 		                   {
-			                   String Availablity = rs.getString("Availablity");
+			                   String Availablity = rs.getString("availablity");
 			                   
 			                   int val=Integer.valueOf(Availablity);
 			                   
 			                   if (val<=0)
 			                   {
-			                	   System.out.println("Not Avaiable");
-			        
+			                	   System.out.println("Out of Stock");
 			                   }
 			                   else
 			                   {
 			                	   val=val-1;
 			                	   
-			                	   String sql1 = "UPDATE wm SET wm.Availablity = ? where wm.Title=?";
+			                	   String sql1 = "UPDATE model SET model.availablity = ? where model.Gunmodel=?";
 			                	   PreparedStatement stmt1 = conn.prepareStatement(sql1);
 			                	   stmt1.setInt(1, val);
 			                	   stmt1.setString(2, input);
 			                	   stmt1.executeUpdate();
 			                	   System.out.println("Enter your Address");
-			                	   String addr= scan.nextLine();
-			                	   String sql2 = "INSERT INTO book Values" + "("+ Id1 + ", " + "'" + input + "', " + "'" + addr + "')";
+			                	   String addr= sc.nextLine();
+			                	   String sql2 = "INSERT INTO model Values" + "("+ Id1 + ", " + "'" + input + "', " + "'" + addr + "')";
 			                	   stmt.execute(sql2);
 			     	
-			                	   System.out.println("Booking Confirmed");
+			                	   System.out.println("Order Placed");
 			                   }
 		                   }
 		                   
 		                   else
 		                   {
-		                	   System.out.println("That model does not exist..."); 
+		                	   System.out.println("This FireArm is not available..."); 
 		                   }
 		                   rs.close();
 		               }
 		               
 		               
-		               else if (answer == 4)
+		               else if (reply == 4)
 		               {
-		            	   String sql = "SELECT Model FROM book where Id = "+Id1+"";
+		            	   String sql = "SELECT Gunmodel FROM LogBook where custid = "+Id1+"";
 		            	   
 		                   try (ResultSet rs = stmt.executeQuery(sql)) {
 		                	   
-		                	   System.out.println("Model No");
+		                	   System.out.println("Gun Model :");
 		                       while(rs.next()){
 		                           //Retrieve by column name
-		                           String title = rs.getString("Model");
+		                           String Gunmodel = rs.getString("Model");
 		                           
 		                           //Display values
-		                           System.out.println(title);
+		                           System.out.println(Gunmodel);
 		                           
 		                       }
 		                       
@@ -208,43 +207,43 @@ public class User {
 		                   }
 		               }
 		               
-		               else if (answer == 5)
+		               else if (reply == 5)
 		               {
 		            	   System.out.println("Dashboard");
 		            	   System.out.println("********************************");
 			               System.out.println("Please select a query below!");
 			               //5 available options
 			               System.out.println("1. Cart");
-			               System.out.println("2. Edit info ");
+			               System.out.println("2. Edit information ");
 			               System.out.println("3. Buy");
 			       
 			               System.out.println("4. Exit from Dashboard");
 			               System.out.println("********************************");
 			               
 			               System.out.println("Enter Your Choice");
-			               String input1 = scan.nextLine();
-			               int answer1;
+			               String input1 = sc.nextLine();
+			               int reply1;
 			               try{
-			            	   answer1=Integer.valueOf(input1);
+			            	   reply1=Integer.valueOf(input1);
 			               }
 			               catch(Exception e)
 			               {
-			            	   answer1=-1;
+			            	   reply1=-1;
 			               }
-			               if(answer1 == -1 || answer1 < 1 || answer1 > 4)
+			               if(reply1 == -1 || reply1 < 1 || reply1 > 4)
 			               {
 			                   //User entered incorrect input
 			                   System.out.println("Incorrect Input!");
 			                   System.out.println("Please enter a number 1-5");
 			               }
 			               
-			               else if (answer1==1)
+			               else if (reply1==1)
 			               {
-			            	   String sql = "SELECT Model FROM book where Id = "+Id1+"";
+			            	   String sql = "SELECT Gunmodel FROM LogBook where custid = "+Id1+"";
 			            	   
 			                   try (ResultSet rs = stmt.executeQuery(sql)) {
 			                	   
-			                	   System.out.println("Model No");
+			                	   System.out.println("FireArm model: ");
 			                       while(rs.next()){
 			                           //Retrieve by column name
 			                           String title = rs.getString("Model");
@@ -264,18 +263,18 @@ public class User {
 			            	   
 			               }
 			               
-			               else if (answer1==3)
+			               else if (reply1==3)
 			               {
-			            	   System.out.println("Enter your Address");
-		                	   String addr= scan.nextLine();
-		                	   String sql2 = "INSERT INTO book Values" + "("+ Id1 + ", " + "'" + input + "', " + "'" + addr + "')";
+			            	   System.out.println("Enter your Address: ");
+		                	   String addr= sc.nextLine();
+		                	   String sql2 = "INSERT INTO LogBook Values" + "("+ Id1 + ", " + "'" + input + "', " + "'" + addr + "')";
 		                	   stmt.execute(sql2);
 		     	
 			               }
 		               }
-		               else if (answer == 6)
+		               else if (reply == 6)
 		               {
-		            	   querying = false;
+		            	   flag = false;
 		            	   
 		               }
 		               
@@ -308,7 +307,7 @@ public class User {
 		         System.out.println("Error closing connection to the database!");
 		      }//end finally try
 		   }//end try
-		   System.out.println("Goodbye!");
+		   System.out.println("Rock'n Roll Baby!");
 
 	}
 
