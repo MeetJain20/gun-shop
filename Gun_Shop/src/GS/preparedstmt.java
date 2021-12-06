58b3797cf94995ff6025af315e8b6a6f3fe562c7
saurabhkumar1432 <75939984+saurabhkumar1432@users.noreply.github.com>
2021-12-06 12:57:22 +0530

    Add files via upload

---
+++ Gun_Shop/src/GS/preparedstmt.java
@@ -0,0 +1,16 @@
+package GS;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public abstract class preparedstmt {
+    public final Connection pstmt(){
+        try {
+            return DriverManager.getConnection("jdbc:mysql://Localhost:3306/firearms", "root", "1432001");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
